a493622b2a871213531af1ea11f93d6e0c33270e
tayan-serna <adrian_serna_92@hotmail.com>
2019-07-09 08:51:48 -0500

    add API request

--- src/characterList.js
+++ src/characterList.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Card from './card';
 
+const RickAndMortyAPI = 'https://rickandmortyapi.com/api/character/';
 class CharacterList extends Component {
   state = {
-    searchValue: ''
+    searchValue: '',
+    characters: []
   };
+
+  componentDidMount() {
+    axios.get(RickAndMortyAPI).then(({ data: { results } }) => {
+      this.setState({
+        characters: results || []
+      });
+    });
+  }
 
   handleChangeInput = e => {
     this.setState({
@@ -14,7 +25,7 @@ class CharacterList extends Component {
   };
 
   render() {
-    const { searchValue } = this.state;
+    const { searchValue, characters } = this.state;
     return (
       <div className="list-container">
         <div className="search-container">
@@ -28,29 +39,13 @@ class CharacterList extends Component {
           </span>
         </div>
         <ul className="card-container">
-          <Card />
-          <Card
-            name="Morty Smith"
-            src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-          />
-          <Card
-            name="Summer Smith"
-            src="https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-          />
-          <Card
-            name="Beth Smith"
-            src="https://rickandmortyapi.com/api/character/avatar/4.jpeg"
-          />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {characters.map(character => (
+            <Card
+              key={character.id}
+              name={character.name}
+              src={character.image}
+            />
+          ))}
         </ul>
       </div>
     );
